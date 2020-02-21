eff631a1c23ae0c38551baef3ed604699a7f25f5
Doru Mihai <doru.mihai@haufe-lexware.com>
2020-02-21 14:58:53 +0200

    Add faster implementation

--- src/20feb/algorithm/Dutzu_Faster.ts
+++ src/20feb/algorithm/Dutzu_Faster.ts
@@ -58,8 +58,6 @@ const getNextBestLibrary = (availableLibraries: Library[], alreadyScanned: numbe
 // const inputData: InputData = readInputData('files/20feb/in/d_tough_choices.txt')
 // const inputData: InputData = readInputData('files/20feb/in/f_libraries_of_the_world.txt')
 const inputData: InputData = readInputData('files/20feb/in/e_so_many_books.txt')
-
-
 // console.log(inputData)
 
 const chosenLibraries: LibraryResult[] = []
@@ -67,10 +65,34 @@ let alreadyScanned: number[] = []
 let daysRemaining = inputData.dayCount
 let currentScore = 0
 
+// while (true) {
+//     let currentLibraryResult: LibraryResult = getNextBestLibrary(inputData.libraries,alreadyScanned,daysRemaining,inputData.bookScores)
+//     if (currentLibraryResult == null) break;
+//     if (currentLibraryResult.scoreYield === 0) break;
+//
+//     chosenLibraries.push(currentLibraryResult)
+//     inputData.libraries.splice(inputData.libraries.indexOf(currentLibraryResult.library),1)
+//
+//     alreadyScanned = alreadyScanned.concat(currentLibraryResult.bookSequence)
+//     currentScore = currentScore + currentLibraryResult.scoreYield
+//     daysRemaining = daysRemaining - currentLibraryResult.library.signupDuration
+//
+//     console.log('Next best library: ', currentLibraryResult.library.id, ' - score: ', currentLibraryResult.scoreYield, '. Remaining days: ', daysRemaining)
+//
+// }
+
+const sortedLibraryYields = inputData.libraries.map(library => libraryPotential(library, alreadyScanned, daysRemaining, inputData.bookScores))
+    .sort((a, b) => {
+        if (a?.scoreYield > b?.scoreYield) return 1
+        if (a?.scoreYield < b?.scoreYield) return -1
+        return 0
+    })
+
 while (true) {
-    let currentLibraryResult: LibraryResult = getNextBestLibrary(inputData.libraries,alreadyScanned,daysRemaining,inputData.bookScores)
+    let currentLibraryResult: LibraryResult = sortedLibraryYields.pop()
     if (currentLibraryResult == null) break;
     if (currentLibraryResult.scoreYield === 0) break;
+    if (daysRemaining - currentLibraryResult.library.signupDuration <= 0) break;
 
     chosenLibraries.push(currentLibraryResult)
     inputData.libraries.splice(inputData.libraries.indexOf(currentLibraryResult.library),1)
